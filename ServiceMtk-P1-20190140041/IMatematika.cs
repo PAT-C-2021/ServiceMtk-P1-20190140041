057813c3224d00f545d23ec91a64eb8529cf65d2
Nando Alhasanain <Nandoalhasanain@gmail.com>
2021-11-22 18:32:58 +0700

    PRAKTIKUM 6

--- ServiceMtk-P1-20190140041/IMatematika.cs
+++ ServiceMtk-P1-20190140041/IMatematika.cs
@@ -12,15 +12,24 @@ namespace ServiceMtk_P1_20190140041
     public interface IMatematika
     {
         [OperationContract]
+        [FaultContract(typeof(MathFault))] 
         /* output->*/
         int Tambah(int a, int b); //method
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Kurang(int a, int b); //input
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Kali(int a, int b);
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Bagi(int a, int b);
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         Koordinat TKoordinat(Koordinat a, Koordinat b); //object dari class
 
     }
@@ -41,6 +50,15 @@ namespace ServiceMtk_P1_20190140041
             get { return _y; }
             set { _y = value; }
         }
+    }
+
+    class MathFault
+    {
+        [DataMember]
+        public string Kode { get; set; }
+
+        [DataMember]
+        public string Pesan { get; set; }
     }
 
 
